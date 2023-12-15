83c4d012e1f3dba3a8017a37a53c7d409e3e10df
Sabbat509 <114038686+Sabbat509@users.noreply.github.com>
2023-12-15 23:12:10 +0800

    Delete GoogleRanking.java

--- GoogleRanking.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class GoogleRanking {
-    public List<String> refineGoogleRank(List<String> googleResults, Map<String, Integer> siteRankings) {
-        // Refine the rank of Google search results using siteRankings
-        return new ArrayList<>();
-    }
-}
