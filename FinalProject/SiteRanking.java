616ce88ebe70cd69c046ec940116b4a22b927c05
Sabbat509 <114038686+Sabbat509@users.noreply.github.com>
2023-12-15 23:04:24 +0800

    Create SiteRanking.java

---
+++ FinalProject/SiteRanking.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SiteRanking {
+    public Map<String, Integer> rankSites(List<String> sites, List<Keyword> keywords) {
+        // Implement site ranking based on keyword appearances
+        return new HashMap<>();
+    }
+}
