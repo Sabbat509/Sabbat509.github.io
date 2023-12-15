2b09351cabeb37b5dc2f4c2d8ba344ec2982dcbb
Sabbat509 <114038686+Sabbat509@users.noreply.github.com>
2023-12-15 23:13:40 +0800

    Delete SemanticsAnalysis.java

--- SemanticsAnalysis.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class SemanticsAnalysis {
-    public List<String> deriveRelativeKeywords(List<String> webSites) {
-        // Implement semantics analysis to derive relative keywords from webSites
-        return new ArrayList<>();
-    }
-}
