72ab18d251e0eb049ecc83f2eb144a430d247367
Sabbat509 <114038686+Sabbat509@users.noreply.github.com>
2023-12-15 23:12:53 +0800

    Delete PageRanking.java

--- PageRanking.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class PageRanking {
-    public List<String> rankPages(List<String> urls, List<Keyword> keywords) {
-        // Implement page ranking based on keywords
-        return new ArrayList<>();
-    }
-}
