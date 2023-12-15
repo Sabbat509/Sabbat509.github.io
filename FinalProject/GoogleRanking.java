4d2d294a6d950fda379aeb720ffa1cf9142c2ec1
Sabbat509 <114038686+Sabbat509@users.noreply.github.com>
2023-12-15 23:03:58 +0800

    Create GoogleRanking.java

---
+++ FinalProject/GoogleRanking.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GoogleRanking {
+    public List<String> refineGoogleRank(List<String> googleResults, Map<String, Integer> siteRankings) {
+        // Refine the rank of Google search results using siteRankings
+        return new ArrayList<>();
+    }
+}
