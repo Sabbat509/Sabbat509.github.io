6881b8d9c5c78811877870adbf912a6bce462add
Sabbat509 <114038686+Sabbat509@users.noreply.github.com>
2023-12-15 23:04:50 +0800

    Create PageRanking.java

---
+++ FinalProject/PageRanking.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageRanking {
+    public List<String> rankPages(List<String> urls, List<Keyword> keywords) {
+        // Implement page ranking based on keywords
+        return new ArrayList<>();
+    }
+}
