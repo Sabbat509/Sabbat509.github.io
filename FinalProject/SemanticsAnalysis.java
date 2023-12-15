b34710e0d2dd573fed63d6dd4ca283fd73663257
Sabbat509 <114038686+Sabbat509@users.noreply.github.com>
2023-12-15 23:03:34 +0800

    Create SemanticsAnalysis.java

---
+++ FinalProject/SemanticsAnalysis.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SemanticsAnalysis {
+    public List<String> deriveRelativeKeywords(List<String> webSites) {
+        // Implement semantics analysis to derive relative keywords from webSites
+        return new ArrayList<>();
+    }
+}
