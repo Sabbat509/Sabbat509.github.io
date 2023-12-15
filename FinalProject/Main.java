790c7d5aa380509d788e6cddb1547130639eae96
Sabbat509 <114038686+Sabbat509@users.noreply.github.com>
2023-12-15 23:01:23 +0800

    Update Main.java

--- FinalProject/Main.java
+++ FinalProject/Main.java
@@ -1,1 +1,21 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Specify search URLs (e.g., google.com and duckduckgo.com)
+        List<String> searchUrls = Arrays.asList("https://www.google.com", "https://duckduckgo.com");
+
+        // Create a SearchEngineService instance
+        SearchEngineService searchEngineService = new SearchEngineService(searchUrls);
+
+        // Build the search database with an external keyword
+        searchEngineService.buildSearchDatabase("restaurants");
+
+        // Perform a search using the mobile application
+        MobileApplication mobileApp = new MobileApplication(searchEngineService);
+        mobileApp.runMobileApp();
+    }
+}
 
